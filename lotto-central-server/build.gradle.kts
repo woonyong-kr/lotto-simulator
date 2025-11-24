d89c1019a84c551e3322f3d3f9a0dc64881b5949
choi woo-nyong <woonyong.kr@gmail.com>
2025-11-24 21:42:50 +0900

    build: 의존성 수정, 환경 변수 등록

--- lotto-central-server/build.gradle.kts
+++ lotto-central-server/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
     implementation(project(":lotto-core"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
