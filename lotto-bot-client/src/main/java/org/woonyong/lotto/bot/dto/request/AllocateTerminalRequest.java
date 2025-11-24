8b15fdb32c1f95abdf6601866f0e6d36c07e3b7c
choi woo-nyong <woonyong.kr@gmail.com>
2025-11-24 22:04:23 +0900

    refactor: WinningNumber 엔티티 및 칼럼 네임 추가
     - 모듈 간 Pos 터미널 할당 프로토콜 정의
     - 분산 시스템 리소스 관리 및 할당

---
+++ lotto-bot-client/src/main/java/org/woonyong/lotto/bot/dto/request/AllocateTerminalRequest.java
@@ -0,0 +1,30 @@
+package org.woonyong.lotto.bot.dto.request;
+
+public class AllocateTerminalRequest {
+    private String botUid;
+    private String posUid;
+
+    public AllocateTerminalRequest() {
+    }
+
+    public AllocateTerminalRequest(final String botUid, final String posUid) {
+        this.botUid = botUid;
+        this.posUid = posUid;
+    }
+
+    public String getBotUid() {
+        return botUid;
+    }
+
+    public void setBotUid(final String botUid) {
+        this.botUid = botUid;
+    }
+
+    public String getPosUid() {
+        return posUid;
+    }
+
+    public void setPosUid(final String posUid) {
+        this.posUid = posUid;
+    }
+}
---
+++ lotto-bot-client/src/main/java/org/woonyong/lotto/bot/dto/response/AllocateTerminalResponse.java
@@ -0,0 +1,43 @@
+package org.woonyong.lotto.bot.dto.response;
+
+public class AllocateTerminalResponse {
+    private String terminalId;
+    private String address;
+    private boolean success;
+    private String message;
+
+    public AllocateTerminalResponse() {
+    }
+
+    public String getTerminalId() {
+        return terminalId;
+    }
+
+    public void setTerminalId(final String terminalId) {
+        this.terminalId = terminalId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(final String address) {
+        this.address = address;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(final boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(final String message) {
+        this.message = message;
+    }
+}
